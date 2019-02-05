7f959291475f3ad5a595ff49a8465617529ea3a7
Piotr Krysik <pkrysik@elka.pw.edu.pl>
2019-02-05 08:47:02 +0100

    runtime: Added sincos function for complex<int16_t>

--- gnuradio-runtime/include/gnuradio/fxpt_nco.h
+++ gnuradio-runtime/include/gnuradio/fxpt_nco.h
@@ -96,6 +96,19 @@ namespace gr {
       }
     }
 
+    // compute cos and sin for a block of phase angles
+    void sincos(std::complex<int16_t> *output, int noutput_items, double ampl=1.0)
+    {
+      for(int i = 0; i < noutput_items; i++) {
+        output[i] = std::complex<int16_t>(
+          static_cast<int16_t>(round(gr::fxpt::cos(d_phase) * ampl)),
+          static_cast<int16_t>(round(gr::fxpt::sin(d_phase) * ampl))
+        );
+        step();
+      }
+    }
+
+
     // compute sin for a block of phase angles
     void sin(float *output, int noutput_items, double ampl=1.0)
     {
